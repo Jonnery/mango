990e84c0e1b870c7a45b790df921d286d375f56a
刘桂林-南宁团队 <827752884@qq.com>
2021-02-25 17:18:24 +0800

    整合swagger

---
+++ src/main/java/com/louis/mango/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.louis.mango.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TODO
+ *
+ * @author Guilin
+ * @version 1.0
+ * @date 2021/2/25 16:02
+ */
+@RestController
+public class HelloController {
+    @GetMapping(value = "/hello")
+    public  Object hello(){return "Hello Mango";}
+}
